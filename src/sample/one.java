8439622d58b210010ec9cc3e56315db0eeb8ca91
coderlucifer420 <root@kali>
2020-01-22 19:39:52 -0500

    New branch

--- src/sample/one.java
+++ src/sample/one.java
@@ -9,19 +9,30 @@ public class one {
 
     public static void main(String[] args) {
         Scanner n= new Scanner(System.in);
-        String movie = null;
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Enter a movie: ");
-            movie= n.nextLine();
-        }
-        for (int i = 0; i < 3; i++) {
-            System.out.println(movie);
-        }
-            System.out.println("Changes");
-            int j=10;
-            for(int k=0; k<j; k++){
-                System.out.printf("*\t#");
+        int ch, a=0, b=0, sum=0, diff=0;
+        do{
+            System.out.println("1.to add");
+            System.out.println("2 to subtract");
+            System.out.println("3. to exit");
+            System.out.print("Enter your choice: ");
+            ch= n.nextInt();
+            switch(ch){
+                case 1: System.out.println("Enter two numbers: ");
+                        a= n.nextInt();
+                        b= n.nextInt();
+                        sum= a+b;
+                        System.out.printf("Sum= %d\n\n", sum);
+                        break;
+                case 2: System.out.println("Enter two numbers: ");
+                        a= n.nextInt();
+                        b= n.nextInt();
+                        diff= a-b;
+                        System.out.printf("Difference= %d\n\n", diff);
+                        break;
+                case 3: System.exit(0);
+                    
+                default: System.out.println("Enter valid choice");
             }
-        System.out.printf("\nNew changes");
+        }while(ch<4);
     }
 }
